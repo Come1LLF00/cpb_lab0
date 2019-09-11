9b921e762cb60a8b6e668eb97b9af9afc3433dae
Come1LLF00 <rayveleaveme34@inbox.ru>
2019-09-12 02:56:22 +0300

    4.5 and 4.6 unabled

--- script.sh
+++ script.sh
@@ -1,42 +1,59 @@
-#1 point
-bash
-mkdir lab0 | cd $_
+#1st point
+mkdir lab0 && cd $_
 mkdir -p barboach2/kricketot larvitar4/wooper raticate3/pineco raticate3/garbodor raticate3/poliwrath raticate3/dodrio raticate3/electabuzz
 touch barboach2/pichu barboach2/ponyta gible8 larvitar4/wynaut larvitar4/lotad poochyena7 raticate3/cofagrigus starly3
-echo "Способности  Charm Thundershock Tail Whip Thunder Wave Sweet\nKiss Nasty Plot" > barboach2/pichu "Способности  Tackle Growl Tail Whip Ember\nFlame Wheel Stomp Flame Charge Fire Spin Take Down Inferno Agility\nFire Blast Bounce Flare Blitz" > barboach2/ponyta "Живёт  Cave\nDesert" > gible8 "Развитые способности  Telepathy" > larvitar4/wynaut "weight=5.7\nheight=20.0 atk=3 def=3" > larvitar4/lotad "weight=30.0 height=20.0 atk=6\ndef=4" > poochyena7 "weigh=168.7 height=67.0 atk=5\ndef=15" > raticate3/cofagrigus "Способности  Growl Tackle Quick Attack Wing Attack\nDouble Team Endeavor Whirlwind Aerial Ace Take Down Agility Brave Bird\nFinal Gambit" > starly3
-#2 point
-chmod 044 barboach2/ponyta	#----r--r--
-chmod 335 raticate3/electabuzz	#d-wx-wxr-x
-chmod 400 barboach2/pichu	#-r--------
-chmod 400 gible8	#-r--------
-chmod 404 raticate3/cofagrigus	#-r-----r--
-chmod 404 larvitar4/lotad	#-r-----r--
-chmod 440 starly3	#r--r-----
-chmod 444 larvitar4/wynaut	#-r--r--r--
-chmod 524 barboach2	#dr-x-w-r--
-chmod 524 raticate3/garbodor	#dr-x-w-r--
-chmod 573 raticate3	#dr-xrwx-wx
-chmod 573 larvitar4/wooper	#dr-xrwx-wx
-chmod 577 kricketot	#dr-xrwxrwx
-chmod 624 poochyena7	#-rw--w-r--
-chmod 737 larvitar4	#drwx-wxrwx
-chmod 752 raticate3/poliwrath	#drwxr-x-w-
-chmod 777 raticate3/pineco	#drwxrwxrwx
-chmod 777 raticate3/dodrio	#drwxrwxrwx
-#3 point
+echo -e 'Способности  Charm Thundershock Tail Whip Thunder Wave Sweet\nKiss Nasty Plot' > barboach2/pichu
+echo -e 'Способности  Tackle Growl Tail Whip Ember\nFlame Wheel Stomp Flame Charge Fire Spin Take Down Inferno Agility\nFire Blast Bounce Flare Blitz' > barboach2/ponyta
+echo -e 'Живёт  Cave\nDesert' > gible8
+echo -e 'Развитые способности  Telepathy' > larvitar4/wynaut
+echo -e 'weight=5.7\nheight=20.0 atk=3 def=3' > larvitar4/lotad
+echo -e 'weight=30/0 height=20.0 atk=6\ndef=4' > poochyena7
+echo -e 'weight=168.7 height=67.0 atk=5\ndef=15' > raticate3/cofagrigus
+echo -e 'Способности  Growl Tackle Quick Attack Wing Attack\nDouble Team Endeavor Whirlwind Aerial Ace Take Down Agility Brave Bird\nFinal Gambit' > starly3
+#2nd point
+chmod 524 barboach2 # text r-x-w-r--
+chmod 577 barboach2/kricketot # text r-xrwxrwx
+chmod 400 barboach2/pichu #r--------
+chmod 044 barboach2/ponyta 
+chmod 400 gible8 # symbols
+
+chmod g-r larvitar4 #symbols
+chmod a+w larvitar4
+
+chmod a=r-- larvitar4/wynaut
+#lotad
+chmod a=r-- larvitar4/lotad
+chmod g-r larvitar4/lotad
+
+chmod 573 larvitar4/wooper #text
+chmod g=w poochyena7
+#raticate3
+chmod u-w raticate3
+chmod go+w raticate3
+chmod g-r raticate3
+
+chmod 404 raticate3/cofagrigus 
+chmod a=rwx raticate3/pineco
+chmod 524 raticate3/garbodor 
+chmod 752 raticate3/poliwrath
+chmod a=rwx raticate3/dodrio
+chmod 335 raticate3/electabuzz
+#starly3
+chmod ug=r-- starly3
+chmod o=--- starly3
+#3rd point
 cp poochyena7 larvitar4/wynautpoochyena
 ln -s raticate3 Copy_20
 cat larvitar4/lotad raticate3/cofagrigus > starly3_16
-ln poochyena7 /home/s284733/lab0/larvitar4/lotadpoochyena
-cp -r larvitar4 barboach2/kricketot
-cp starly3 raticate3/electabuzz
-ln -s poochyena7 barboach2/pichupoochyena
-#4 point
-#5 point
-rm poochyena7
-rmdir larvitar4
-cd ..
-rm lab0/larvitar4/lotad
-rm # TODO: 3-th dot
-rm lab0/larvitar4/lotadpoochye* # TODO: 4-th dot
-rmdir lab0/raticate3/pineco
+ln poochyena7 larvitar4/lotadpoochyena
+cp -r larvitar4 barboach2/kricketot/larvitar4 # permission denied
+cp starly3 raticate3/electabuzz/starly3
+ln -s poochyena7 barboach2/pichupoochyena # permission denied
+#4th point
+wc -m raticate3/* | sort -nr 2>/tmp/errors.txt #1
+ls -al * | sort -nrk 5 | grep "3$" #2
+cat poochyena7 | grep -i "y$" 2>&1 #3
+ls -alr * | grep "co" | sort -mr | tail -4 #4
+ls -r * | grep "o$" | sort -ur | cat -n 2>&1 #5
+ls -alr * | grep "^g\*" | sort -m 2>&1 #6
+#5th point
